576942a0e9c3c9e63a266de4f1469f97d2fd9ba0
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-06-16 16:13:23 +0900

    Title: 배열 채우기, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250616/배열 채우기/filling-array.py
+++ 250616/배열 채우기/filling-array.py
@@ -1,5 +1,7 @@
 arr = list(map(int,input().split()))
-arr = arr[:10]
-arr = [a for a in arr if a != 0]
-for i in arr[::-1]:
+idx = 10
+for i in range(len(arr)):
+    if arr[i] == 0:
+        idx = i
+for i in arr[idx-1::-1]:
     print(i,end=" ")
