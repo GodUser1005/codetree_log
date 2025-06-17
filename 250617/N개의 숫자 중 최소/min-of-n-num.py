5a7a98f797b7219c0aa64beb6f84896dc1dc6779
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-06-17 11:00:06 +0900

    Title: N개의 숫자 중 최소, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250617/N개의 숫자 중 최소/min-of-n-num.py
+++ 250617/N개의 숫자 중 최소/min-of-n-num.py
@@ -4,10 +4,11 @@ a = list(map(int, input().split()))
 # Please write your code here.
 min_val = a[0]
 cnt = 1
-for i in a:
+for i in a[1:]:
     if min_val > i:
         min_val = i
         cnt = 1
     elif min_val == i:
         cnt += 1
+
 print(min_val,cnt)
