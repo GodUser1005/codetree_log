84a700e28103f434d2c7a315373b5b82c50aecab
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-06-17 11:50:27 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 55ms, Memory: 17MB, Status: Passed - Codetree

--- 250617/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 250617/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -4,11 +4,9 @@ a = list(map(int, input().split()))
 # Please write your code here.
 max_idx = n
 while max_idx > 0:
-    max_val = a[0]
     max_idx = 0
     for i in range(1,len(a)):
-        if a[i] > max_val:
-            max_val = a[i]
+        if a[i] > a[max_idx]:
             max_idx = i
     print(max_idx + 1,end=" ")
     a = a[:max_idx]
