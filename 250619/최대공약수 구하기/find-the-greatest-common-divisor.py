68611f73cbf09446885baeb6ebebc37bcc798c4d
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-06-19 14:53:22 +0900

    Title: 최대공약수 구하기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250619/최대공약수 구하기/find-the-greatest-common-divisor.py
+++ 250619/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -1,11 +1,19 @@
-def gcd(n,m):
+def euclid_gcd(n,m):
     if n < m:
         n,m = m,n
     if m == 0:
         return n
     return gcd(m,n%m)
 
+def gcd(n,m):
+    gcd = 1
+    for i in range(1,min(n,m)+1):
+        if n % i == 0 and m % i == 0:
+            gcd = i
+    return gcd
+
 n, m = map(int, input().split())
 
 # Please write your code here.
+# print(euclid_gcd(n,m))
 print(gcd(n,m))
