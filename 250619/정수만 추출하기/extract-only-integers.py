c92b20db60f96d0108261e2b1c097e9abe452761
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-06-19 10:52:41 +0900

    Title: 정수만 추출하기, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250619/정수만 추출하기/extract-only-integers.py
+++ 250619/정수만 추출하기/extract-only-integers.py
@@ -5,11 +5,15 @@ if not a.isdigit():
         if not a[i].isdigit():
             a = int(a[:i])
             break
+else:
+    a = int(a)
     
 if not b.isdigit():
     for i in range(len(b)):
         if not b[i].isdigit():
             b = int(b[:i])
             break
+else:
+    b = int(b)
 
 print(a+b)
