7fe0cdc2acafb4aa7275ba6660a6d6ca58eca079
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-06-19 16:33:24 +0900

    Title: 사칙연산 함수, Time: 78ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250619/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,25 @@
+def is_operation(o):
+    if o in '+/-*':
+        return True
+    return False
+
+def operate(o,a,c):
+    if o == '+':
+        return a + c
+    elif o == '-':
+        return a - b
+    elif o == '*':
+        return a * c
+    elif o == '/':
+        return a // c
+
+
+a, o, c = input().split()
+a = int(a)
+c = int(c)
+
+# Please write your code here.
+if is_operation(o):
+   print(f"{a} {o} {c} = {operate(o,a,c)}") 
+else:
+    print('False')
