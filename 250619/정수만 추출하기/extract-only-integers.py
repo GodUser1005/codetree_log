e6d87420dc6a13a7494e830eb68c676e98394d88
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-06-19 10:51:38 +0900

    Title: 정수만 추출하기, Time: 73ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250619/정수만 추출하기/extract-only-integers.py
+++ 250619/정수만 추출하기/extract-only-integers.py
@@ -1,14 +1,15 @@
 a,b = input().split()
 
-for i in range(len(a)):
-    if not a[i].isdigit():
-        a = int(a[:i])
-        break
+if not a.isdigit():
+    for i in range(len(a)):
+        if not a[i].isdigit():
+            a = int(a[:i])
+            break
     
-
-for i in range(len(b)):
-    if not b[i].isdigit():
-        b = int(b[:i])
-        break
+if not b.isdigit():
+    for i in range(len(b)):
+        if not b[i].isdigit():
+            b = int(b[:i])
+            break
 
 print(a+b)
