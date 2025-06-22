61b9bde1858c9e453cc91b62d7adffe87e4c8ddd
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-06-23 01:38:37 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250623/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -0,0 +1,18 @@
+def lcm(arr):
+    prod = 1
+    for e in arr:
+        prod *= e
+    if prod == 1:
+        return 1
+    for d in [2,3,5,7]:
+        if prod % d == 0:
+            for i in range(len(arr)):
+                if arr[i] % d == 0:
+                    arr[i] //= d
+            return d * lcm(arr)
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+print(lcm(arr))
