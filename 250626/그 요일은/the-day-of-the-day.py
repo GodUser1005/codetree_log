87b6b3a7c3e74d6b2b4e19392819ac6425a1870d
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-06-26 15:40:49 +0900

    Title: 그 요일은, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

--- 250626/그 요일은/the-day-of-the-day.py
+++ 250626/그 요일은/the-day-of-the-day.py
@@ -3,10 +3,16 @@ A = input()
 
 # Please write your code here.
 days = [0,31,29,31,30,31,30,31,31,30,31,30,31]
+week_of_days = ["Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
 sum_days = [0]
 for i in range(12):
     sum_days.append(sum_days[i] + days[i+1])
 
 diff = (sum_days[m2-1]+d2)-(sum_days[m1-1]+d1)
+result = diff // 7
+for i in range(diff % 7 + 1):
+    if week_of_days[i] == A:
+        result += 1
+        break
 
-print(diff // 7 + 1)
+print(result)
