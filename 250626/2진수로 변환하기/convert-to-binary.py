b4f961e3a97a8f7e3553912dbf36f87f4cf8c310
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-06-26 15:52:46 +0900

    Title: 2진수로 변환하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250626/2진수로 변환하기/convert-to-binary.py
+++ 250626/2진수로 변환하기/convert-to-binary.py
@@ -3,11 +3,10 @@ n = int(input())
 # Please write your code here.
 def decimal_to_digit(n):
     digit = []
-    while n > 2:
+    while n >= 2:
         digit.append(str(n % 2))
         n //= 2
-        if n < 2:
-            digit.append(str(n))
+    digit.append(str(n))
     digit = digit[::-1]
     return "".join(digit)
 
