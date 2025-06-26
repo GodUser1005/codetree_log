217d811dfe3bef059169a959f332cce90aeb9746
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-06-26 10:53:26 +0900

    Title: 폭탄 해제, Time: 82ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250626/폭탄 해제/bomb-removal.py
@@ -0,0 +1,12 @@
+unlock_code, wire_color, seconds = input().split()
+seconds = int(seconds)
+
+# Please write your code here.
+class Bomb:
+    def __init__(self,unlock_code,wire_color,seconds):
+        self.unlock_code = unlock_code
+        self.color = color
+        self.seconds = seconds
+
+bomb = Bomb(unlock_code,wire_color,seconds)
+print(f"code : {bomb.unlock_code}\ncolor : {bomb.wire_color}\nsecond : {bomb.seconds}")
