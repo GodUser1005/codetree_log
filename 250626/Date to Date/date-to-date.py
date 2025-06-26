95fdbb6e53345e1725e53a6d386cdf2ea47a44c2
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-06-26 14:24:50 +0900

    Title: Date to Date, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250626/Date to Date/date-to-date.py
@@ -0,0 +1,9 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+# Please write your code here.
+days = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+sum_days = [0]
+for i in range(12):
+    sum_days.append(sum_days[i] + days[i+1])
+
+print(sum_days[m2-1]+d2 - (sum_days[m1-1]+d1) + 1)
