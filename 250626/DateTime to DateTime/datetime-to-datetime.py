130f9d90eb87368d95da847d2973c5ca1bc70141
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-06-26 14:35:09 +0900

    Title: DateTime to DateTime, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250626/DateTime to DateTime/datetime-to-datetime.py
+++ 250626/DateTime to DateTime/datetime-to-datetime.py
@@ -2,4 +2,4 @@ a, b, c = map(int, input().split())
 
 # Please write your code here.
 time = ((a-11)*60*24 + b * 60 + c) - (11*60 + 11)
-print(time if time > 0 else -1)
+print(time if time >= 0 else -1)
