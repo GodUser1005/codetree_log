9c8390aa27e6bb50dda50e868ada4be9f83db066
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-06-26 15:20:32 +0900

    Title: 그 요일은, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250626/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,12 @@
+m1, d1, m2, d2 = map(int, input().split())
+A = input()
+
+# Please write your code here.
+days = [0,31,29,31,30,31,30,31,31,30,31,30,31]
+sum_days = [0]
+for i in range(12):
+    sum_days.append(sum_days[i] + days[i+1])
+
+diff = (sum_days[m2-1]+d2)-(sum_days[m1-1]+d1)
+
+print(diff//7 + 1 if A != "Mon" else diff//7 + 2)
