f9bdd672b9f7f71b8c50ef641421fce35c10898c
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-07-07 22:43:16 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 83ms, Memory: 47MB, Status: Wrong Answer - Codetree

--- 250707/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 250707/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -1,8 +1,6 @@
 x1, y1, x2, y2 = [0] * 2, [0] * 2, [0] * 2, [0] * 2
 x1[0], y1[0], x2[0], y2[0] = map(int, input().split())
 x1[1], y1[1], x2[1], y2[1] = map(int, input().split())
-# x1[0], y1[0], x2[0], y2[0] =-8, 7, 5, 9
-# x1[1], y1[1], x2[1], y2[1] = -3, -2, -2 ,-1
 
 # Please write your code here.
 offset = 1000
@@ -21,14 +19,14 @@ def find_bottom_left():
         for j in range(x1[0]+offset,x2[0]+offset):
             if mat[i][j] == 0:
                 return j,i
-    return x2[0]+offset, y2[0]+offset
+    return -1,-1
 
 def find_up_right():
     for i in range(y2[0]+offset-1,y1[0]+offset-1,-1):
         for j in range(x2[0]+offset-1,x1[0]+offset-1,-1):
             if mat[i][j] == 0:
                 return j+1,i+1
-    return x1[0] + offset, y1[0]+offset
+    return -1,-1
 
 bottom_left = find_bottom_left()
 up_right = find_up_right()
@@ -36,23 +34,24 @@ up_right = find_up_right()
 rec_length = x2[0] - x1[0]
 rec_height = y2[0] - y1[0]
 
-length = up_right[0] - bottom_left[0]
-height = up_right[1] - bottom_left[1]
+blank_length = up_right[0] - bottom_left[0]
+blank_height = up_right[1] - bottom_left[1]
 
-if rec_length != length and rec_height != height:
-    print(rec_length * rec_height)
-elif rec_height == height and rec_length == length:
+if blank_length < rec_length and blank_height < rec_height:
+    print(rec_length*rec_height)
+elif blank_length == rec_length and blank_height == rec_height:
     print(0)
-elif rec_length == length:
-    if bottom_left[1] != (y1[0] + offset) and up_right[1] != y2[0] + offset:
-        print(rec_length * rec_height)
+elif blank_length == rec_length and blank_height < rec_height:
+    if bottom_left[1] != y1[0] and up_right[1] != y2[0]:
+        print(rec_length*rec_height)
     else:
-        print(rec_length * (rec_height-height))
-elif rec_height == height:
-    if bottom_left[1] != (x1[0] + offset) and up_right[1] != x2[0] + offset:
-        print(rec_length * rec_height)
+        print(rec_length * (rec_height - blank_height))
+elif blank_length < rec_length and blank_height == rec_height:
+    if bottom_left[0] != x1[0] and up_right[0] != x2[0]:
+        print(rec_length*rec_height)
     else:
-        print((rec_length-length) * rec_height)
+        print((rec_length - blank_length) * rec_height)
+
 
 # for i in range(y1[0]+offset,y2[0]+offset):
 #     for j in range(x1[0]+offset,x2[0]+offset):
