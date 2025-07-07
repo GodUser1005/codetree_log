328784ea2eb07a39fcd8636373bcb99810b21145
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-07-07 22:24:58 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 82ms, Memory: 47MB, Status: Wrong Answer - Codetree

--- 250707/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 250707/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -1,8 +1,8 @@
 x1, y1, x2, y2 = [0] * 2, [0] * 2, [0] * 2, [0] * 2
 x1[0], y1[0], x2[0], y2[0] = map(int, input().split())
 x1[1], y1[1], x2[1], y2[1] = map(int, input().split())
-# x1[0], y1[0], x2[0], y2[0] =-19, -16, 17, 1
-# x1[1], y1[1], x2[1], y2[1] = -68, -79, 17, 11
+# x1[0], y1[0], x2[0], y2[0] =-8, 7, 5, 9
+# x1[1], y1[1], x2[1], y2[1] = -3, -2, -2 ,-1
 
 # Please write your code here.
 offset = 1000
@@ -21,14 +21,14 @@ def find_bottom_left():
         for j in range(x1[0]+offset,x2[0]+offset):
             if mat[i][j] == 0:
                 return j,i
-    return x1[0]+offset, y1[0]+offset
+    return x2[0]+offset, y2[0]+offset
 
 def find_up_right():
     for i in range(y2[0]+offset-1,y1[0]+offset-1,-1):
         for j in range(x2[0]+offset-1,x1[0]+offset-1,-1):
             if mat[i][j] == 0:
                 return j+1,i+1
-    return x2[0] + offset, y2[0]+offset
+    return x1[0] + offset, y1[0]+offset
 
 bottom_left = find_bottom_left()
 up_right = find_up_right()
