9416d08f283e3727ba69173a1d4bc986a111c351
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-07-10 09:39:13 +0900

    Title: 문자에 따른 명령 2, Time: 65ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250710/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,19 @@
+dirs = input()
+
+# Please write your code here.
+dx = [0,1,0,-1]
+dy = [1,0,-1,0]
+
+pos = [0,0]
+look_at_index = 0
+
+for dir in dirs:
+    if dir == 'L':
+        look_at_index = (look_at_index + 3) % 4
+    elif dir == 'R':
+        look_at_index = (look_at_index + 1) % 4
+    elif dir == 'F':
+        pos[0] += pos[0] + dx[look_at_index]
+        pos[1] += pos[1] + dy[look_at_index]
+
+print(pos[0],pos[1])
