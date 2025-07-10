5b06154baa91b178d8122abbcb4f91dc69fbd6b9
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-07-10 11:01:52 +0900

    Title: 작은 구슬의 이동, Time: 53ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250710/작은 구슬의 이동/small-marble-movement.py
@@ -0,0 +1,33 @@
+def switch_direction(d):
+    if d == 'D':
+        return 'U'
+    elif d == 'U':
+        return 'D'
+    elif d == 'R':
+        return 'L'
+    elif d == 'L':
+        return 'R'
+
+n,t = map(int,input().split())
+r,c,d = input().split()
+r,c = map(int,(r,c))
+
+directions = {
+    'U':(1,0),
+    'D':(-1,0),
+    'R':(0,1),
+    'L':(0,-1)
+}
+
+for _ in range(t):
+    tmp = [r,c]
+    tmp[0] += directions[d][0]
+    tmp[1] += directions[d][1]
+    if 0 < tmp[0] < n+1 and 0 < tmp[1] < n+1:
+        r,c = tmp
+    else:
+        d = switch_direction(d)
+
+print(r,c)
+
+    
