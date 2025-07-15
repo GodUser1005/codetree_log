cc017eaa871b517b746598cf9cbae260e6a345f3
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-07-15 21:19:12 +0900

    Title: 이상한 진수 2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250715/이상한 진수 2/awkward-digits-2.py
+++ 250715/이상한 진수 2/awkward-digits-2.py
@@ -1,10 +1,14 @@
 a = list(input())
 
 # Please write your code here.
+change = False
 for i in range(1,len(a)):
     if a[i] == '0':
         a[i] = '1'
+        change = True
         break
+if not change:
+    a[len(a)-1] = '0'
 
 ans = 0
 for i in range(len(a)):
