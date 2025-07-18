72adfb4fbc1dd2fc553bb664c5c42adc15c5fe54
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-07-18 11:19:47 +0900

    Title: 스승의 은혜 3, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250718/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -0,0 +1,23 @@
+n,b = map(int,input().split())
+p = []
+s = []
+for i in range(n):
+    pi,si = map(int,input().split())
+    p.append(pi)
+    s.append(si)
+
+max_count = 0
+for i in range(n):
+    temp = p[:]
+    temp[i] //= 2
+    total_cost = [temp[j] + s[j] for j in range(n)]
+    total_cost.sort()
+    tmp_b = b
+    c = 0
+    for j in range(1,n+1):
+        tmp_b -= total_cost[j-1]
+        if tmp_b > 0:
+            c = j
+    max_count = max(c,max_count)
+
+print(max_count)
