bdd35d01ac7e5ef83506083976a63a487ba4ba77
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-07-18 09:22:58 +0900

    Title: 상해버린 치즈, Time: 67ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250718/상해버린 치즈/rotten-cheese.py
@@ -0,0 +1,21 @@
+n,m,d,s = map(int,input().split())
+cheese_time_list = [tuple(map(int,input().split())) for _ in range(d)]
+sick_time_list = [tuple(map(int,input().split())) for _ in range(s)]
+
+decay_prob = [0] * (m + 1)
+
+for i in range(s):
+    for j in range(d):
+        if sick_time_list[i][0] == cheese_time_list[j][0]:
+            if sick_time_list[i][1] > cheese_time_list[j][2]:
+                decay_prob[cheese_time_list[j][1]] += 1
+
+sick_prob = [0] * (n+1)
+for i in range(1,m+1):
+    if decay_prob[i] == s:
+        for j in range(d):
+            if cheese_time_list[j][1] == i:
+                sick_prob[cheese_time_list[j][0]] = 1
+
+print(sum(sick_prob))
+
