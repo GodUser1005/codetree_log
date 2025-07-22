46bcffd2239f982c59759542b9cd80339bb5db87
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-07-23 02:58:00 +0900

    Title: 팀으로 하는 틱택토 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250723/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 250723/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -1,13 +1,11 @@
-inp = [input() for _ in range(3)]
+inp = [list(map(int,list(input()))) for _ in range(3)]
 
 # Please write your code here.
 
-count = 0
-
-def win(a,b):
-    satisfied = True
+def row_win(a,b):
     for r in range(3):
-        if inp[r][0] == inp[r][1] == inp[r][2]:
+        satisfied = True
+        if inp[r][0] == inp[r][1] and inp[r][0] == inp[r][2]:
             continue
         for c in range(3):
             if inp[r][c] not in [a,b]:
@@ -15,8 +13,11 @@ def win(a,b):
                 break
         if satisfied:
             return True
-    
+    return False
+
+def col_win(a,b):
     for c in range(3):
+        satisfied = True
         if inp[0][c] == inp[1][c] == inp[2][c]:
             continue
         for r in range(3):
@@ -25,12 +26,39 @@ def win(a,b):
                 break
         if satisfied:
             return True
-    
+    return False
+
+def cross_win(a,b):
+    l = list(set([inp[0][0],inp[1][1],inp[2][2]]))
+    l.sort()
+    if l == [a,b]:
+        return True
+    l = list(set([inp[2][0],inp[1][1],inp[0][2]]))
+    l.sort()
+    if l == [a,b]:
+        return True
+    return False
     
 
+def win(a,b):
+    if row_win(a,b):
+        return True
+    elif col_win(a,b):
+        return True
+    elif cross_win(a,b):
+        return True
+    return False
+    
+    
+    
+    
+count = 0
 for i in range(1,10):
     for j in range(i+1,10):
-        a = 5
+        if win(i,j):
+            count += 1
+
+print(count)
                     
 
         
