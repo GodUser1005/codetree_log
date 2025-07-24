4451f0f5ae4c08af2ad451b9cd3b379fb4373b92
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-07-24 15:00:26 +0900

    Title: 훌륭한 점프, Time: 49ms, Memory: 16MB, Status: Passed - Codetree

--- 250724/훌륭한 점프/great-jump.py
+++ 250724/훌륭한 점프/great-jump.py
@@ -3,7 +3,7 @@ arr = list(map(int, input().split()))
 
 # Please write your code here.
 ans = 0
-for i in range(max(arr),max([arr[0],arr[-1]])-1,-1):
+for i in range(max(arr),max(arr[0],arr[-1])-1,-1):
     jump_cnt = k - 1
     for num in arr:
         jump_cnt -= 1
@@ -14,5 +14,6 @@ for i in range(max(arr),max([arr[0],arr[-1]])-1,-1):
             break
     if ans > 0:
         break
+ans = max(arr[-1],arr[0],ans)
     
 print(ans)
