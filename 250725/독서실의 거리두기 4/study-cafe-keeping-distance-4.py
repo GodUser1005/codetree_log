55c3cc33c2fb874b0b84ad19ef0f7044621b7e63
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-07-25 12:27:31 +0900

    Title: 독서실의 거리두기 4, Time: 61ms, Memory: 17MB, Status: Passed - Codetree

--- 250725/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
+++ 250725/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
@@ -9,10 +9,10 @@ for i in range(n):
             if seat[j] == 0:
                 tmp_seat = seat.copy()
                 tmp_seat[i],tmp_seat[j] = 1,1
-                seat_pos = [i for i in range(len(tmp_seat)) if tmp_seat[i] == 1]
+                seat_pos = [k for k in range(len(tmp_seat)) if tmp_seat[k] == 1]
                 min_dist = n
-                for i in range(len(seat_pos)-1):
-                    dist = seat_pos[i+1]-seat_pos[i]
+                for k in range(len(seat_pos)-1):
+                    dist = seat_pos[k+1]-seat_pos[k]
                     min_dist = min(dist,min_dist)
                 max_near_dist = max(max_near_dist,min_dist)
 
