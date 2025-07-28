b3bfa32825d07400bffe61d8ddf905d472b2f657
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-07-28 16:24:35 +0900

    Title: 구간 잘 나누기, Time: 52ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250728/구간 잘 나누기/divide-sections-well.py
+++ 250728/구간 잘 나누기/divide-sections-well.py
@@ -12,7 +12,7 @@ for i in range(max_sum,max(a)-1,-1):
         if tmp_sum > i:
             count -= 1
             tmp_sum = a[j]
-    if count < 0:
+    if count == -1:
         ans = i+1
         break
 print(ans)
