48ef906be39617056bd1e29dec83d33076fc9250
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-07-28 16:18:05 +0900

    Title: 구간 잘 나누기, Time: 54ms, Memory: 17MB, Status: Wrong Answer - Codetree

--- 250728/구간 잘 나누기/divide-sections-well.py
+++ 250728/구간 잘 나누기/divide-sections-well.py
@@ -4,7 +4,7 @@ a = list(map(int, input().split()))
 # Please write your code here.
 ans = 0
 max_sum = sum(a)
-for i in range(max_sum,0,-1):
+for i in range(max_sum,max(a)-1,-1):
     tmp_sum = 0
     count = m - 1
     for j in range(n):
