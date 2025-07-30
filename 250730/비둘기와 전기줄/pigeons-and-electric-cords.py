7c8a5adf5e18fd5c2199b87609d24c262c5e2ae5
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-07-30 19:40:38 +0900

    Title: 비둘기와 전기줄, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250730/비둘기와 전기줄/pigeons-and-electric-cords.py
+++ 250730/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -5,8 +5,10 @@ n = int(input())
 
 for _ in range(n):
     b,p = map(int,input().split())
-    if (birds[b] == 0 and p == 1) or (birds[b] == 1 and p == 0):
+    if birds[b] == -1:
+        birds[b] = p
+    elif birds[b] != p:
         counts[b] += 1
-    birds[b] = p
+        birds[b] = p
 
 print(sum(counts))
