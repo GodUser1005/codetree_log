5091bc6cebab6c9f2a5544748e533e9e98abed4b
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-07-31 14:25:07 +0900

    Title: 독서실의 거리두기 2, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250731/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -0,0 +1,18 @@
+n = int(input())
+seats = list(input())
+
+# Please write your code here.
+positions = [i for i,seat in enumerate(seats) if seat == '1']
+diff = [positions[i+1]-positions[i] for i in range(len(positions)-1)]
+if 1 in diff:
+    print(1)
+else:
+    diff.sort()
+    min_diff = diff[-1] // 2
+    max_near_diff = 0
+    if len(diff) >= 2:
+        min_diff = min(min_diff,diff[0])
+        max_near_diff = min_diff
+    if seats[n-1] == '0':
+        min_diff = min(diff[-1],n-1 - positions[-1])
+    print(max(max_near_diff,min_diff))
