22fd7a21bbe471125fff2bb81386bbda9d138cf2
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-07-31 13:05:24 +0900

    Title: 개발자의 가위바위보, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250731/개발자의 가위바위보/developers-rock-paper-scissors.py
+++ 250731/개발자의 가위바위보/developers-rock-paper-scissors.py
@@ -1,17 +1,17 @@
 n = int(input())
 logs = [tuple(map(int,input().split())) for _ in range(n)]
 
-count_1 = 0
-count_2 = 0
+count_win = 0
+count_draw = 0
 for a,b in logs:
-    if a == 3 and b == 1:
-        count_1 += 1
+    if a == b:
+        count_draw += 1
+    elif a == 3 and b == 1:
+        count_win += 1
     elif b > a:
-        count_1 += 1
-    
-    if b == 3 and a == 1:
-        count_2 += 1
-    elif a > b:
-        count_2 += 1
+        count_win += 1
 
-print(max(count_1,count_2))
+count_win = max(count_win,n - count_draw - count_win)
+print(count_win)
+    
+    
