a9074b85c6fc45d04c327da753d804de7734fc69
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-07-31 14:45:21 +0900

    Title: 독서실의 거리두기 2, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250731/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
+++ 250731/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -3,21 +3,24 @@ seats = list(input())
 
 # Please write your code here.
 positions = [i for i,seat in enumerate(seats) if seat == '1']
-diff = [positions[i+1]-positions[i] for i in range(len(positions)-1)]
-if 1 in diff:
-    print(1)
+if len(positions) == 1:
+    print(max(positions[0],n-1 - positions[0]))
 else:
-    diff.sort()
-    nearest_diff = diff[-1]//2
-    if len(diff) >= 2:
-        nearest_diff = min(nearest_diff,diff[0])
-    if seats[-1] == '1':
-        print(nearest_diff)
+    diff = [positions[i+1]-positions[i] for i in range(len(positions)-1)]
+    if 1 in diff:
+        print(1)
     else:
-        min_diff = min(diff[0],n-1 - positions[-1])
-        if seats[0] == '0':
-            min_diff_2 = min(diff[0],positions[0])
-            print(max(nearest_diff,min_diff,min_diff_2))
+        diff.sort()
+        nearest_diff = diff[-1]//2
+        if len(diff) >= 2:
+            nearest_diff = min(nearest_diff,diff[0])
+        if seats[-1] == '1':
+            print(nearest_diff)
         else:
-            print(max(nearest_diff,min_diff))
+            min_diff = min(diff[0],n-1 - positions[-1])
+            if seats[0] == '0':
+                min_diff_2 = min(diff[0],positions[0])
+                print(max(nearest_diff,min_diff,min_diff_2))
+            else:
+                print(max(nearest_diff,min_diff))
     
