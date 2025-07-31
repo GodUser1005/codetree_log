7885b1e592ad625e508ecc94241a110b75df3b5e
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-07-31 10:28:26 +0900

    Title: X 달리기, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 250731/X 달리기/x-run.py
+++ 250731/X 달리기/x-run.py
@@ -6,11 +6,13 @@ def cal_min_time(x):
     while True:
         sum_to_max_v = sum(range(1,max_v+1))
         min_dist = sum_to_max_v * 2 - max_v
-        max_dist = sum_to_max_v * 2
+        max_dist = sum_to_max_v * 2 + max_v
         if min_dist == x:
             return 2*max_v-1
-        elif min_dist < x <= max_dist:
-            return 2*max_v
+        elif min_dist < x <= sum_to_max_v*2:
+            return 2 * max_v
+        elif x <= max_dist:
+            return 2 * max_v + 1
         max_v += 1
 
 print(cal_min_time(x))
