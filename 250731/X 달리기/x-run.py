410cd0cec24abf4a3cd5d567d53662b7fa29e635
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-07-31 09:48:01 +0900

    Title: X 달리기, Time: 1084ms, Memory: 17MB, Status: Time Limit Exceed - Codetree

--- 250731/X 달리기/x-run.py
+++ 250731/X 달리기/x-run.py
@@ -1,23 +1,21 @@
 x = int(input())
 
 # Please write your code here.
-def cal_max_v(x):
+def cal_min_time(x):
     max_v = 1
     while True:
-        dist = 0
-        for i in range(1,max_v+1):
-            dist += i
-        dist = 2*dist - max_v
-        if dist > x:
-            return max_v - 1
+        sum_to_max_v = sum(range(1,max_v+1))
+        min_dist = sum_to_max_v * 2 - max_v
+        max_dist = sum_to_max_v * 2
+        if min_dist == x:
+            return 2*max_v-1
+        elif min_dist < x <= max_dist:
+            return 2*max_v
         max_v += 1
 
-max_v = cal_max_v(x)
-dist = sum(range(1,max_v+1))*2 - max_v
-ans = (2 * max_v - 1)
-if dist < x:
-    ans += 1
+print(cal_min_time(x))
     
-print(ans)
+
+
             
         
