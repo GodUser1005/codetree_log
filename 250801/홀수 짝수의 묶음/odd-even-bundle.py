3fc1d78a64508c3748a2f2e1cf8e052fb5e955d3
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-08-01 11:16:00 +0900

    Title: 홀수 짝수의 묶음, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250801/홀수 짝수의 묶음/odd-even-bundle.py
@@ -0,0 +1,17 @@
+n = int(input())
+numbers = list(map(int, input().split()))
+
+# Please write your code here.
+odd_count = len([num for num in numbers if num % 2 == 1])
+even_count = n - odd_count
+
+basket_count = min(odd_count,even_count) * 2
+if odd_count > even_count:
+    odd_count -= even_count
+    if odd_count % 3 == 0:
+        basket_count += (odd_count // 3) * 2
+    elif odd_count % 3 == 1:
+        basket_count += (odd_count // 3) * 2 - 1
+    elif odd_count % 3 == 2:
+        basket_count += ((odd_count // 3) * 2 + 1)
+print(basket_count)
