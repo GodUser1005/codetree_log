3385c5d24377bb4cc52941454d931d51bf1f489d
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-08-01 23:46:08 +0900

    Title: 전부 포함하는 선분, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250801/전부 포함하는 선분/all-inclusive-segment.py
+++ 250801/전부 포함하는 선분/all-inclusive-segment.py
@@ -2,10 +2,42 @@ n = int(input())
 segments = [tuple(map(int, input().split())) for _ in range(n)]
 
 # Please write your code here.
+x1,x2 = zip(*segments)
 
-segments.sort(key = lambda x: (x[0],x[1]))
+min_x1 = 100
+min_x1_id = 0
+max_x2 = 0
+max_x2_id = 100
+for i in range(n):
+    if min_x1 > x1[i]:
+        min_x1 = x1[i]
+        min_x1_id = i
+    if max_x2 < x2[i]:
+        max_x2 = x2[i]
+        max_x2_id = i
 
-cand_1 = segments[-1][1] - segments[1][0]
-cand_2 = segments[-2][1] - segments[0][0]
+min_x1 = 100
+max_x2 = 0
+for i in range(n):
+    if i == min_x1_id:
+        continue
+    if min_x1 > x1[i]:
+        min_x1 = x1[i]
+    if max_x2 < x2[i]:
+        max_x2 = x2[i]
+ans = max_x2 - min_x1
 
-print(min(cand_1,cand_2))
+min_x1 = 100
+max_x2 = 0
+for i in range(n):
+    if i == max_x2_id:
+        continue
+    if min_x1 > x1[i]:
+        min_x1 = x1[i]
+    if max_x2 < x2[i]:
+        max_x2 = x2[i]
+
+ans = min(ans,max_x2 - min_x1)
+
+print(ans)
+
