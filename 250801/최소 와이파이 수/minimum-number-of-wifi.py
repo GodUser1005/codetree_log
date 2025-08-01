f47dfaca79cde17953b2d064dc04bed6f0372950
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-08-01 10:46:38 +0900

    Title: 최소 와이파이 수, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250801/최소 와이파이 수/minimum-number-of-wifi.py
@@ -0,0 +1,16 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+count = 0
+tmp = arr[:]
+
+i = 0
+while i < n:
+    if tmp[i] == 1:
+        pos = i + 2*m
+        count += 1
+        i += pos
+    i += 1
+
+print(count)
