6d1165cef84ae8ae5027658e658c29b9adfb6680
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-08-05 03:58:05 +0900

    Title: 황금비율 토스트, Time: 193ms, Memory: 34MB, Status: Runtime Error - Codetree

--- 250805/황금비율 토스트/golden-toast.py
+++ 250805/황금비율 토스트/golden-toast.py
@@ -7,13 +7,15 @@ class Node:
 
 class DoublyLinkedList():
     def __init__(self):
-        self.head = None
+        self.head = Node('nil')
         self.tail = None
         self.size = 0
     
     def push_back(self,data):
         new_node = Node(data)
         if self.size == 0:
+            self.head.next = new_node
+            new_node.prev = self.head
             self.head = new_node
             self.tail = new_node
         else:
@@ -27,12 +29,62 @@ class DoublyLinkedList():
     
     def end(self):
         return self.tail
-    
-dll = DoublyLinkedList()
-dll.push_back(Node('c'))
-dll.push_back(Node('c'))
-dll.push_back(Node('c'))
 
+n,m = map(int,input().split())
+bread = input()
+dll = DoublyLinkedList()
+for c in bread:
+    dll.push_back(c)
+
+iterator = dll.end()
+
+commands = []
+for _ in range(m):
+    commands.append(list(input().split()))
+
+for command in commands:
+    if len(command) == 1:
+        c, = command
+        if c == 'L':
+            if iterator == dll.begin().prev:
+                continue
+            iterator = iterator.prev
+        elif c == 'R':
+            if iterator == dll.end():
+                continue
+            iterator = iterator.next
+        elif c == 'D':
+            if iterator == dll.end():
+                continue
+            del_node = iterator.next
+            if del_node == dll.end():
+                dll.tail = iterator
+                iterator.next = None
+            else:
+                iterator.next = del_node.next
+                del_node.next.prev = iterator
+                if del_node == dll.begin():
+                    dll.head = iterator.next
+            dll.size -= 1
+    else:
+        c,d = command
+        new_node = Node(d)
+        new_node.prev = iterator
+        new_node.next = iterator.next
+        iterator.next = new_node
+        if new_node.next != None:
+            new_node.next.prev = new_node
+        iterator = new_node
+        dll.size += 1
+
+iterator = dll.begin()
+for _ in range(dll.size):
+    print(iterator.data,end="")
+    iterator = iterator.next
+
+
+        
+        
 
 
 
