dbf9db12cb9489e354ef4243ae1183972b6111b0
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-08-21 11:25:58 +0900

    Title: 기수 정렬 구현, Time: 166ms, Memory: 27MB, Status: Passed - Codetree

---
+++ 250821/기수 정렬 구현/implement-radix-sort.py
@@ -0,0 +1,23 @@
+def ith_digit(num,i):
+    return int(f"{num:0>6d}"[i])
+
+def radix_sort(arr):
+    for i in range(5,-1,-1):
+        list_arr = [[] for _ in range(10)]
+        for num in arr:
+            list_arr[ith_digit(num,i)].append(num)
+
+        new_arr = []
+        for j in range(10):
+            row_list = list_arr[j]
+            for k in range(len(row_list)):
+                new_arr.append(row_list[k])
+        arr = new_arr
+    return arr
+
+n = int(input())
+arr = list(map(int, input().split()))
+answer = " ".join(list(map(str,radix_sort(arr))))
+
+# Please write your code here.
+print(answer)
