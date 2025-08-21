5d1e4de3a1f796de800b5be7bed99fb6945fdf3f
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-08-21 14:45:22 +0900

    Title: 병합 정렬 구현, Time: 76ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 250821/병합 정렬 구현/implement-merge-sort.py
@@ -0,0 +1,33 @@
+def merge(left,right):
+    left_i, right_i = 0,0
+    new_arr = []
+    while not (left_i == len(left) or right_i == len(right)):
+        left_num = left[left_i]
+        right_num = right[right_i]
+        if left_num < right_num:
+            new_arr.append(left_num)
+            left_i += 1
+        else:
+            new_arr.append(right_num)
+            right_i += 1
+    
+    if left_i == len(left):
+        new_arr += right[right_i:]
+    else:
+        new_arr += left[left_i:]
+    return new_arr
+
+
+
+def merge_sort(arr):
+    n = len(arr)
+    left = merge_sort(arr[:n/2])
+    right = merge_sort(arr[n/2:])
+    arr = merge(left,right)
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+sorted_arr = merge_sort(arr)
+print(" ".join(list(map(str,sorted_arr))))
