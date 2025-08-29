5e9ccd077aa3a12020d6a2477354558c5d65b744
jijone1005 <62534719+GodUser1005@users.noreply.github.com>
2025-08-29 12:33:18 +0900

    Title: 원형 순열에서의 인원 제거, Time: 105ms, Memory: 26MB, Status: Passed - Codetree

--- 250829/원형 순열에서의 인원 제거/josephus-permutations.py
+++ 250829/원형 순열에서의 인원 제거/josephus-permutations.py
@@ -6,7 +6,8 @@ q = deque()
 for i in range(1,n+1):
     q.append(i)
 
-while len(q) != 0:
+while len(q) != 1:
     for i in range(k-1):
         q.append(q.popleft())
     print(q.popleft(),end=" ")
+print(q.popleft())
